c9601809152d611dcfdfefe75dd4dcbcf33095bd
«Ваше <fr33lancemail@gmail.com>
2024-04-05 21:04:45 +0200

    Add style/partial question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: %i[show edit update destroy]
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
