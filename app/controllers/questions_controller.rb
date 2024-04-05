b6304394558b28c2d9f07a195c4cdda97cf46ce8
«Ваше <fr33lancemail@gmail.com>
2024-04-05 20:08:57 +0200

    Update controller, templates/+Minimal styling app

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,42 @@
+# frozen_string_literal: true
+
 class QuestionsController < ApplicationController
-  def create
-    Question.create(
-      body: params[:question][:body],
-      user_id: params[:question][:user_id]
-    )
-
-    redirect_to '/'
-  end
-
+  before_action :set_question, only: %i[show edit update destroy]
   def index
     @questions = Question.all
   end
 
-  def show
-    @question = Question.find(params[:id])
+  def new
+    @question = Question.new
   end
 
+  def create
+    @question = Question.create(question_params)
+
+    redirect_to question_path(@question)
+  end
+
+  def show; end
+
+  def edit; end
+
   def update
-    @question = Question.find(params[:id])
-    @question.update(
-      body: params[:question][:body],
-      user_id: params[:question][:user_id]
-    )
+    @question.update(question_params)
+    redirect_to question_path(@question)
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
