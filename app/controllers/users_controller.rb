55e19da7ec3cb0235f4f10dbd3b2beef644b2aaa
«Ваше <fr33lancemail@gmail.com>
2024-04-15 20:29:39 +0200

    CRUD & Validates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,43 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
   def create
-    user_params = params[:user].permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    User.create(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'User was successfully created.'
+    else
+      flash.now[:alert] = 'User was not created. Invalid data.'
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-    redirect_to root_path, notice: 'User was successfully created.'
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      redirect_to root_path, notice: 'User was successfully updated.'
+    else
+      flash.now[:alert] = 'User was not updated. Invalid data.'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: 'User was successfully destroyed.'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  before_save :downcase_nickname
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'is not valid' }
+  validates :name, presence: true
+  validates :nickname, presence: true, uniqueness: true
   has_secure_password
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
